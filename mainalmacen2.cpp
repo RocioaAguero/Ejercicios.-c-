e0e547d077c69c60e59ef6bde7ea7659be55d805
RocioaAguero <65718124+RocioaAguero@users.noreply.github.com>
2020-09-01 12:58:55 -0300

    Create mainalmacen2.cpp

---
+++ mainalmacen2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+#include "almacen2.hpp"
+#include "almacen2.cpp"
+
+int main(){
+    cout<<"\t\t\t\tActualizaciones de inventario \n"<< endl;
+    
+    producto_t azucar = {"Azucar", "Ledesma", 100, 60.5F};
+    
+    
+
+    return 0;
+}
